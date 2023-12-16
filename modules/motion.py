c414785c763690bf0353e724169bb0c382cf9f89
KDW <42798237+dawan0111@users.noreply.github.com>
2023-12-16 13:22:39 +0900

    update macos version

--- modules/motion.py
+++ modules/motion.py
@@ -10,9 +10,7 @@ def xor_and_add(data):
 
 class Motion:
   def __init__(self):
-    self.servo = None
-    self.safety_thr = 20
-    self.ser = serial.Serial('/dev/ttyS0', 115200, timeout=1)
+    pass
 
   def send_smooth_motion(self, start_motion, goal_motion):
       # x y z
@@ -39,28 +37,7 @@ class Motion:
       self.ser.write(goal_motion)  
 
   def run(self, motions):
-      for motion in motions:
-          print("motion: {}".format(motion))
-          motion_data = bytearray([0xFF, 0xFF, 90, 90, 90, 0x00, 0x00, 0x00])
-          reset_data = bytearray([0xFF, 0xFF, 90, 90, 90, 0x00, 0x00, 0x00])
-          if motion == "L":
-              motion_data[3] += self.safety_thr
-          elif motion == "R":
-              motion_data[3] -= self.safety_thr
-          elif motion == "U":
-              motion_data[2] += self.safety_thr
-          elif motion == "D":
-              motion_data[2] -= self.safety_thr
-          elif motion == "L_E":
-              motion_data[5] = 0x01
-          elif motion == "R_E":
-              motion_data[6] = 0x01
-          
-          print("motion_data start!!")
-          self.send_smooth_motion(reset_data, motion_data)
-          time.sleep(1)
-          self.send_smooth_motion(motion_data, reset_data)
-          time.sleep(1)
+      pass
 
 if __name__ == "__main__":
     motion = Motion()
